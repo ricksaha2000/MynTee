4dc64615c9921c91f6fec4e666f6abfae8b69195
Jayit Saha <jayit.saha2018@vitstudent.ac.in>
2021-10-30 03:57:14 +0530

    Chatbot

---
+++ chatbot1/data/shopping-list/rasa/Untitled-1.js
@@ -0,0 +1,13 @@
+create database users;
+use users;
+create table user if not exists (id int(10) primary key, username varchar(50), password varchar(50));
+insert into user values(1,'jayit','jayit123');
+insert into user values(2,'saha','saha123');
+insert into user values(3,'jayit','18bce1071');
+insert into user values(4,'jay','narayan');
+insert into user values(5,'soma','saha');
+insert into user values(6,'admin','jayitsaha');
+insert into user values(7,'rai','rai123');
+insert into user values(9,'pogo','wifi');
+insert into user values(10,'amazing','isaa');
+
