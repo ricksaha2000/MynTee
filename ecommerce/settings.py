7169a141de70509fe20f18d19dcea5e226004b3c
Jayit Saha <jayit.saha2018@vitstudent.ac.in>
2021-11-06 01:15:08 +0530

    Virtual Tryon Notebook merge

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-# import fasttext
+import fasttext
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -159,8 +159,8 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[(os.path.join(BASE_DIR,'ecommerce/static'))]
 
-# model1 = fasttext.load_model("amazon.ftz")
-model1 = 'amazon'
+model1 = fasttext.load_model("amazon.ftz")
+# model1 = 'amazon'
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
